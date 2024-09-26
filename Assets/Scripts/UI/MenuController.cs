85ba0a14ef15bc6b33ffd40908b6505c34d41464
jami <nikwunu@gmail.com>
2024-09-26 13:58:54 +0300

    UItest.unity näkyy current level

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -49,6 +49,6 @@ public class MenuController : MonoBehaviour
 
     public void QuitToMenu()
     {
-        SceneManager.LoadScene("UItest2");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
